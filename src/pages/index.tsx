98b05ee8b057e3ddfb631dda5bebc9f5b670ec52
penpen <yamaa.exception@gmail.com>
2021-11-20 21:19:51 +0900

    更新日、投稿日の表示方法の変更

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,8 +2,8 @@ import type { GetStaticProps, NextPage } from "next";
 import Link from "next/link";
 import Layout from "src/components/templates/Layout";
 import { getSortedPostsData } from "src/lib/posts";
-import {CreatedAt} from "src/components/atoms/CreatedAt"
-import {UpdatedAt} from "src/components/atoms/UpdatedAt"
+import { CreatedAt } from "src/components/atoms/CreatedAt";
+import { UpdatedAt } from "src/components/atoms/UpdatedAt";
 import { SEO } from "src/components/molecules/SEO";
 import { config } from "src/config";
 
@@ -12,14 +12,14 @@ type Props = {
     createdAt: string;
     title: string;
     id: string;
-    updatedAt?:string
+    updatedAt?: string;
   }[];
 };
 
 const Home: NextPage<Props> = ({ allPostsData }) => {
   return (
     <>
-      <SEO title={config.siteTitle}  />
+      <SEO title={config.siteTitle} />
       <Layout>
         {allPostsData.map(({ id, createdAt, title, updatedAt }) => (
           <article key={id}>
@@ -27,11 +27,10 @@ const Home: NextPage<Props> = ({ allPostsData }) => {
               <a className="block align-text-bottom mb-10 p-3 hover:bg-gray-300">
                 <h2 className="mb-8">{title}</h2>
                 <div className="text-right">
-                  {updatedAt ? (
+                  {updatedAt !== createdAt && (
                     <UpdatedAt updatedAt={updatedAt}></UpdatedAt>
-                  ) : (
-                    <CreatedAt createdAt={createdAt}></CreatedAt>
                   )}
+                  <CreatedAt createdAt={createdAt}></CreatedAt>
                 </div>
               </a>
             </Link>
